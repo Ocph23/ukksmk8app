b11d18ec1c8988dad0aba44542203ea577f60e99
br3ndly <brendlyrustyone@gmail.com>
2024-02-27 15:10:48 +0900

    layout

---
+++ resources/views/jurusan.blade.php
@@ -0,0 +1,5 @@
+
+@extends('admin')
+@section('content')
+    <h1>Jurusan 1</h1>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,39 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin', function () {
+    return view('admin');
+});
+
+Route::get('/admin/jurusan', function () {
+    return view('jurusan');
+});
+
+Route::get('/admin/tahunajaran', function () {
+    return view('tahunajaran');
+});
+
+Route::get('/admin/aksesor', function () {
+    return view('aksesor');
+});
+
+Route::get('/admin/siswa', function () {
+    return view('siswa');
+});
+
+Route::get('/admin/paket', function () {
+    return view('paket');
+});
+
+Route::get('/admin/penilaian/{id}', function () {
+    return view('penilaian');
+});
+
+
+
+
